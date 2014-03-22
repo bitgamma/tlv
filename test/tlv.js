dc873998376e2ca67d540ebf398e0bdaf38bcf09
Michele Balistreri <michele@briksoftware.com>
2014-03-22 17:33:39 +0100

    added TLV prototype

--- test/tlv.js
+++ test/tlv.js
@@ -25,11 +25,14 @@
 describe('TLV', function() {
   var chai = require('chai').should();
   var tlv = require('../lib/tlv');
+  var TLV = tlv.TLV;
   
   describe('#parse', function() {
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length on 1 byte.', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length on 1 byte.', function() {
       var buf = new Buffer([0x80, 0x04, 0xCA, 0xFE, 0xBA, 0xBE]);
       var res = tlv.parse(buf);
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x80);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(6);
@@ -38,15 +41,17 @@ describe('TLV', function() {
       res.value.should.deep.equal(new Buffer([0xCA, 0xFE, 0xBA, 0xBE]));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length 0x00', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length 0x00', function() {
       var res = tlv.parse(new Buffer([0x80, 0x00]));
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x80);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(2);
       res.value.should.deep.equal(new Buffer([]));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length 0x7F', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length 0x7F', function() {
       var buf = new Buffer(129);
       buf[0] = 0x80;
       buf[1] = 0x7F
@@ -56,13 +61,15 @@ describe('TLV', function() {
       }
       
       var res = tlv.parse(buf);
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x80);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(buf.length);
       res.value.should.deep.equal(buf.slice(2, buf.length));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length on 2 bytes', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length on 2 bytes', function() {
       var buf = new Buffer(131);
       buf[0] = 0xC4;
       buf[1] = 0x81;
@@ -73,13 +80,15 @@ describe('TLV', function() {
       }
       
       var res = tlv.parse(buf);
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0xC4);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(buf.length);
       res.value.should.deep.equal(buf.slice(3, buf.length));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length on 3 bytes and spurious data at end', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length on 3 bytes and spurious data at end', function() {
       var buf = new Buffer(0x109);
       buf[0] = 0x80;
       buf[1] = 0x82;
@@ -91,13 +100,15 @@ describe('TLV', function() {
       }
       
       var res = tlv.parse(buf);
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x80);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal((buf.length - 5));
       res.value.should.deep.equal(buf.slice(4, (buf.length - 5)));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length on 4 bytes and spurious data at end', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length on 4 bytes and spurious data at end', function() {
       var buf = new Buffer(0x1000A);
       buf[0] = 0x12;
       buf[1] = 0x83;
@@ -110,13 +121,15 @@ describe('TLV', function() {
       }
       
       var res = tlv.parse(buf);
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x12);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(buf.length - 5);
       res.value.should.deep.equal(buf.slice(5, (buf.length - 5)));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 1 byte and length on 5 bytes and spurious data at end', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 1 byte and length on 5 bytes and spurious data at end', function() {
       var buf = new Buffer(0x100000B);
       buf[0] = 0x80;
       buf[1] = 0x84;
@@ -124,19 +137,19 @@ describe('TLV', function() {
       buf[3] = 0x00;
       buf[4] = 0x00;
       buf[5] = 0x00;
-      
-      for (i = 6; i < buf.length; i++) {
-        buf[i] = i;
-      }
-      
+      buf[6] = 0xFF;
+      buf[buf.length - 1] = 0xFF;
+
       var res = tlv.parse(buf);
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x80);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(buf.length - 5);
       res.value.should.deep.equal(buf.slice(6, (buf.length - 5)));
     });
     
-    it('should return throw an exception when provided a byte array with primitive tag on 1 byte and length on 6 bytes', function() {
+    it('should throw an exception when provided a buffer with primitive tag on 1 byte and length on 6 bytes', function() {
       var buf = new Buffer(0x1000007);
       buf[0] = 0x80;
       buf[1] = 0x85;
@@ -149,43 +162,75 @@ describe('TLV', function() {
       (function(){ tlv.parse(buf); }).should.throw(RangeError);
     });
     
-    it('should return a TLV object when provided a byte array with constructed tag on 1 byte and length on 1 byte.', function() {
+    it('should return a TLV object when provided a buffer with constructed tag on 1 byte and length on 1 byte.', function() {
       var res = tlv.parse(new Buffer([0xE1, 0x08, 0x80, 0x02, 0xBA, 0xBE, 0x82, 0x02, 0xBB, 0xBC]));
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0xE1);
       res.constructed.should.equal(true);
       res.encodedLength.should.equal(10);
-      res.value.should.deep.equal([{tag: 0x80, value: new Buffer([0xBA, 0xBE]), constructed: false, encodedLength: 4}, {tag: 0x82, value: new Buffer([0xBB, 0xBC]), constructed: false, encodedLength: 4}]);
-    });
-    
-    it('should return a TLV object when provided a byte array with constructed tlvs with 2 levels of nesting', function() {
-      var res = tlv.parse(new Buffer([0xE1, 0x0A, 0xA0, 0x04, 0x82, 0x02, 0xCA, 0xFE, 0x83, 0x02, 0xBB, 0xBC]));
-      res.tag.should.equal(0xE1);
-      res.constructed.should.equal(true);
-      res.encodedLength.should.equal(12);
-      res.value.should.deep.equal([{
-        tag: 0xA0, 
-        value: [ {tag: 0x82, value: new Buffer([0xCA, 0xFE]), constructed: false, encodedLength: 4} ], 
-        constructed: true, 
-        encodedLength: 6
-      }, 
-      { tag: 0x83, value: new Buffer([0xBB, 0xBC]), constructed: false, encodedLength: 4}
+      res.value.should.deep.equal([
+        new TLV(0x80, new Buffer([0xBA, 0xBE]), 4),
+        new TLV(0x82, new Buffer([0xBB, 0xBC]), 4)
       ]);
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 2 bytes and length on 2 bytes.', function() {
+    it('should return a TLV object when provided a buffer with constructed tag on 1 byte and zero length.', function() {
+      var res = tlv.parse(new Buffer([0xE1, 0x00]));
+      
+      res.should.be.an.instanceof(TLV);
+      res.tag.should.equal(0xE1);
+      res.constructed.should.equal(true);
+      res.encodedLength.should.equal(2);
+      res.value.should.deep.equal([]);
+    });
+    
+    it('should return a TLV object when provided a buffer with constructed tlvs with 2 levels of nesting', function() {
+      var res = tlv.parse(new Buffer([0xE1, 0x0A, 0xA0, 0x04, 0x82, 0x02, 0xCA, 0xFE, 0x83, 0x02, 0xBB, 0xBC]));
+      
+      res.should.be.an.instanceof(TLV);
+      res.tag.should.equal(0xE1);
+      res.constructed.should.equal(true);
+      res.encodedLength.should.equal(12);
+      res.value.should.deep.equal([
+        new TLV(0xA0, [new TLV(0x82, new Buffer([0xCA, 0xFE]), 4)], 6),
+        new TLV(0x83, new Buffer([0xBB, 0xBC]), 4)
+      ]);
+    });
+    
+    it('should return a TLV object when provided a buffer with primitive tag on 2 bytes and length on 2 bytes.', function() {
       var res = tlv.parse(new Buffer([0x9F, 0x70, 0x81, 0x04, 0xCA, 0xFE, 0xBA, 0xBE]));
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x9F70);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(8);
       res.value.should.deep.equal(new Buffer([0xCA, 0xFE, 0xBA, 0xBE]));
     });
     
-    it('should return a TLV object when provided a byte array with primitive tag on 3 bytes and length on 2 bytes.', function() {
+    it('should return a TLV object when provided a buffer with primitive tag on 3 bytes and length on 2 bytes.', function() {
       var res = tlv.parse(new Buffer([0x9F, 0x85, 0x22, 0x81, 0x04, 0xCA, 0xFE, 0xBA, 0xBE]));
+      
+      res.should.be.an.instanceof(TLV);
       res.tag.should.equal(0x9F8522);
       res.constructed.should.equal(false);
       res.encodedLength.should.equal(9);
       res.value.should.deep.equal(new Buffer([0xCA, 0xFE, 0xBA, 0xBE]));
+    });
+    
+    it('should return a TLV object when provided a buffer with primitive tag on 4 bytes and length on 2 bytes.', function() {
+      var res = tlv.parse(new Buffer([0x1F, 0x85, 0xA2, 0x01, 0x81, 0x04, 0xCA, 0xFE, 0xBA, 0xBE]));
+      
+      res.should.be.an.instanceof(TLV);
+      res.tag.should.equal(0x1F85A201);
+      res.constructed.should.equal(false);
+      res.encodedLength.should.equal(10);
+      res.value.should.deep.equal(new Buffer([0xCA, 0xFE, 0xBA, 0xBE]));
+    });
+    
+    it('should throw an exception when provided a buffer with primitive tag on 5 bytes.', function() {
+      var buf = new Buffer([0x1F, 0x85, 0xA2, 0x81, 0x01, 0x00]);
+      (function(){ tlv.parse(buf); }).should.throw(RangeError);
     });
   });
 });
